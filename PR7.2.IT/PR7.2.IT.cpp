c3f596c5442920e9c987506348e919ed3e8071a0
Volodymyr Vysotskyi <vova.vysot2.0@gmail.com>
2022-11-17 12:54:59 +0300

    1th commit master

---
+++ PR7.2.IT/PR7.2.IT.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void Create(int** mas, const int rowCount, int  colCount, int A, int B) {
+
+	for (int i = 0; i < rowCount; i++) {
+		for (int j = 0; j < colCount; j++) {
+			mas[i][j] = A + rand() % (B - A + 1);
+		}
+	}
+}
+void Print(int** mas, const int rowCount, int  colCount) {
+
+	for (int i = 0; i < rowCount; i++) {
+		for (int j = 0; j < colCount; j++) {
+			cout << setw(4) << mas[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void Trans(int** mas, const int rowCount, int  colCount) {
+	int t = 0;
+	for (int i = 0; i < colCount; i++) {
+		for (int j = i + 1; j < colCount; j++) {
+			t = mas[i][j];
+			mas[i][j] = mas[j][i];
+			mas[j][i] = t;
+		}
+	}
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int n;
+	cout << "n = "; cin >> n;
+	int rowCount = n;
+	int colCount = rowCount;
+
+	int** T = new int* [rowCount];
+	for (int i = 0; i < rowCount; i++) {
+		T[i] = new int[colCount];
+	}
+	Create(T, rowCount, colCount, -17, 14);
+	cout << "T =" << endl << "{ " << endl;
+	Print(T, rowCount, colCount);
+	cout << "             }" << endl;
+
+	Trans(T, rowCount, colCount);
+	cout << "transponovane T = { " << endl;
+	Print(T, rowCount, colCount);
+	cout << "    }" << endl;
+
+	for (int i = 0; i < rowCount; i++)
+		delete[]T[i];
+	delete[]T;
+
+	return 0;
+}
